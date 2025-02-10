fadb32f3eab21ff7792c17621634a613040f59c5
HDash <16350928+HDash@users.noreply.github.com>
2025-02-10 11:46:36 +0000

    Add descriptions to the publications

--- frontend/src/pages/PublicationsPage.jsx
+++ frontend/src/pages/PublicationsPage.jsx
@@ -6,9 +6,11 @@ import BackgroundAnimation from "../components/BackgroundAnimation";
 
 const { Content } = Layout;
 
+// Description ~50words
 const publications = [
     {
         title: "Harnessing generative AI to annotate the severity of all phenotypic abnormalities within the Human Phenotype Ontology",
+        description: "Using GPT-4, we systematically annotated the severity of all Human Phenotype Ontology (HPO) phenotypes based on clinical characteristics and frequency. Benchmarking showed high accuracy (89–100% recall). A novel severity scoring system enables prioritization of the most detrimental phenotypes, aiding gene therapy selection and therapeutic intervention efforts.",
         authors: "Kitty B Murphy, Brian M Schilder, Nathan G Skene",
         journal: "medRxiv",
         year: 2024,
@@ -16,6 +18,7 @@ const publications = [
     },
     {
         title: "Identification of cell type-specific gene targets underlying thousands of rare diseases and subtraits",
+        description: "Rare diseases are understudied due to their diversity and low prevalence. Using the Human Phenotype Ontology and single-cell transcriptomics, we conducted >688,000 enrichment tests, identifying >13,888 cell type-phenotype associations. We introduce a pipeline for gene target prioritization and provide R packages to enhance accessibility.",
         authors: "Kitty B. Murphy, Robert Gordon-Smith, Jai Chapman, Momoko Otani, Brian M. Schilder, Nathan G. Skene",
         journal: "medRxiv",
         year: 2023,
@@ -41,7 +44,8 @@ export default function PublicationsPage() {
                     <div className="space-y-8 font-mono" style= {{ marginLeft: "3.4em" }}>
                         {publications.map((pub, index) => (
                             <div key={index} className="border-b pb-7">
-                                <h2 className="text-xl font-bold mb-1">{pub.title}</h2>
+                                <p className="font-bold mb-1 text-xl">{pub.title}</p>
+                                <p className="mb-2 text-sm">{pub.description}</p>
                                 <div className="ml-3">
                                     <p><strong>Authors:</strong> {pub.authors}</p>
                                     <p><strong>Journal:</strong> {pub.journal}</p>
