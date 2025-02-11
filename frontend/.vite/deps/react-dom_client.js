c4a6e7e7c72d9e491c13527682b0684ef58fbd7d
HDash <16350928+HDash@users.noreply.github.com>
2025-02-11 13:18:24 +0000

    Untrack .vite

--- frontend/.vite/deps/react-dom_client.js
+++
@@ -1,8 +0,0 @@
-import {
-  require_client
-} from "./chunk-CIW2JBMH.js";
-import "./chunk-YNDS2U4Y.js";
-import "./chunk-65KY755N.js";
-import "./chunk-V4OQ3NZ2.js";
-export default require_client();
-//# sourceMappingURL=react-dom_client.js.map
