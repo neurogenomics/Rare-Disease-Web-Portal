91b2a2c986eb99d5d5bce4cb723cb851e0df6930
HDash <16350928+HDash@users.noreply.github.com>
2025-02-17 10:14:57 +0000

    Automatically set API URL based on build mode

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,10 @@
-// Run in local
-export const BASE_API_URL = "http://127.0.0.1:8000"; // URL of the backend API
-
-// Run in server
-// export const BASE_API_URL = "https://neurogenomics-ukdri-api.dsi.ic.ac.uk"; // URL of the backend API
-
+// Ontology API
 export const ONTOLOGY_API_URL = "https://ontology.jax.org"; // DO NOT change this, this is a third-party API providing ontology data
+
+// Backend API
+const LOCAL_API_URL = "http://127.0.0.1:8000"; // Dev, local
+const SERVER_API_URL = "https://neurogenomics-ukdri-api.dsi.ic.ac.uk"; // Prod
+
+export const BASE_API_URL = import.meta.env.DEV
+    ? LOCAL_API_URL
+    : SERVER_API_URL;
